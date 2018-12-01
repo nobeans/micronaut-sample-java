cce4ae2e074ea6e56f82caebc005ef348def0280
Yasuharu Nakano <ynak@jggug.org>
2018-12-01 15:54:50 +0900

    Return a plain text in body

--- src/main/java/micronaut/sample/HelloController.java
+++ src/main/java/micronaut/sample/HelloController.java
@@ -1,14 +1,14 @@
 package micronaut.sample;
 
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.HttpStatus;
 
 @Controller("/hello")
 public class HelloController {
 
-    @Get("/")
-    public HttpStatus index() {
-        return HttpStatus.OK;
+    @Get(produces = MediaType.TEXT_PLAIN) // producesを指定しない場合、デフォルトはJSON
+    public String index() {
+        return "Hello, Micronaut!";
     }
 }
